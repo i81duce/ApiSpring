97abfdeb97463c669fce01e59839e5926947b559
KevinBozic <bozic.kevin@gmail.com>
2016-03-31 11:35:02 -0400

    Add code

---
+++ src/main/java/com/theironyard/Controller.java
@@ -0,0 +1,26 @@
+package com.theironyard;//Created by KevinBozic on 3/31/16.
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+@RestController
+public class Controller {
+    static final String SAMPLE_URL = "http://gturnquist-quoters.cfapps.io/api/random";
+
+    @RequestMapping(path = "/quote", method = RequestMethod.GET)
+    public HashMap getQuote() {
+        RestTemplate query = new RestTemplate();
+        HashMap result = query.getForObject(SAMPLE_URL, HashMap.class);
+        String type = (String) result.get("type");
+        if (type.equals("success")) {
+            HashMap value = (HashMap) result.get("value");
+            return value;
+        }
+        return null;
+    }
+}
